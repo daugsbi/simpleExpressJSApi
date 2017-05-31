d7adcbcbf6a96d5c2cce7f17d3440ec171e00e46
Denis Augsburger <d.augsbi@gmail.com>
2017-05-31 17:51:34 +0200

    Install express and configure morgan to log every request

---
+++ app.js
@@ -0,0 +1,18 @@
+/**
+ * Backend for an imaginary insurance app
+ */
+const express = require('express');
+var bodyParser = require('body-parser');
+const requestLogger = require('morgan');
+const app = express();
+
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+// Log all requests
+app.use(requestLogger('combined'));
+
+// finally start server
+app.listen(3000, function () {
+  console.log('Imaginary insurance API is listening on port 3000!');
+});
